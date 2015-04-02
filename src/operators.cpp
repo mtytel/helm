a932e86845647c6cc556a3807cd9c36c46f0f472
mtytel <matthewtytel@gmail.com>
2015-04-02 14:25:03 -0700

    Fixed precision bugs in LinearSmoothBuffer.

--- src/operators.cpp
+++ src/operators.cpp
@@ -139,7 +139,7 @@ namespace mopo {
 
   void LinearSmoothBuffer::process() {
     mopo_float new_value = input()->source->buffer[0];
-    if (new_value == last_value_ && output()->buffer[0] == new_value)
+    if (new_value == last_value_ && last_start_value_ == new_value)
       return;
 
     mopo_float inc = (new_value - last_value_) / buffer_size_;
@@ -148,5 +148,7 @@ namespace mopo {
       last_value_ += inc;
       output()->buffer[i] = last_value_;
     }
+    last_start_value_ = last_value_;
+    last_value_ = new_value;
   }
 } // namespace mopo
