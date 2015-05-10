3ad4f14b4ea2bb5b8b855c290aaa19f5583db929
Matt Tytel <matthewtytel@gmail.com>
2015-05-10 12:26:46 -0700

    Renamed plugin project from Twytch to TwtychPlugin.

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -38,7 +38,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "Twytch";
+    const char* const  projectName    = "TwytchPlugin";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
