e342d5a70c01c19cd236ae81d2d5ce8cb94163a8
mtytel <matthewtytel@gmail.com>
2015-07-21 15:33:33 -0700

    Added a hacked portamento slope.

---
+++ src/portamento_slope.h
@@ -0,0 +1,48 @@
+/* Copyright 2013-2015 Matt Tytel
+ *
+ * mopo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mopo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mopo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef PORTAMENTO_SLOPE_H
+#define PORTAMENTO_SLOPE_H
+
+#include "value.h"
+
+namespace mopo {
+
+  // A processor that will slope to the target value over a given amount of
+  // time. Useful for portamento or smoothing out values.
+  class PortamentoSlope : public Processor {
+    public:
+      enum Inputs {
+        kTarget,
+        kRunSeconds,
+        kTriggerJump,
+        kNumInputs
+      };
+
+      PortamentoSlope();
+      virtual ~PortamentoSlope() { }
+
+      virtual Processor* clone() const { return new PortamentoSlope(*this); }
+      virtual void process();
+      void tick(int i);
+
+    private:
+      mopo_float last_value_;
+  };
+} // namespace mopo
+
+#endif // PORTAMENTO_SLOPE_H
