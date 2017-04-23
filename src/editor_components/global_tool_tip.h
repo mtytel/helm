dded9050b848aad401ed9ef7ba68c7343275780c
mtytel <matthewtytel@gmail.com>
2017-04-23 19:14:12 -0400

    Updated license year.

--- src/editor_components/global_tool_tip.h
+++ src/editor_components/global_tool_tip.h
@@ -1,4 +1,4 @@
-/* Copyright 2013-2016 Matt Tytel
+/* Copyright 2013-2017 Matt Tytel
  *
  * helm is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
